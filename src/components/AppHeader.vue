c71c29180d7509ee52d3c7514e7bf670be28ddc3
SergioBuzi <sergio.buzi97@gmail.com>
2023-12-21 17:06:56 +0100

    config: setup base scaffolding

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+    export default{
+        name:"AppHeader",
+    }
+</script>
+
+<template>
+    <h1>qui ci va l header</h1>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+
+
+
+</style>
